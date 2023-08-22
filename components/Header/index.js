5d5656a15bfb5cb860596a8d2ea1c0dc76d12f41
Luis Landa <luislanda@Luiss-MacBook-Pro.local>
2023-08-22 13:19:03 +0200

    Correcting grammar

--- components/Header/index.js
+++ components/Header/index.js
@@ -4,7 +4,7 @@ import styled from "styled-components";
 export default function Header() {
 	return (
 		<HeaderContainer>
-			<Name>Andrés Alegria</Name>
+			<Name>Andrés Alegría</Name>
 			<Title>Graphics Specialist</Title>
 		</HeaderContainer>
 	);
